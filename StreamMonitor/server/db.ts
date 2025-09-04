249b067f26a24a23d8e1bd062ca68624003a3bab
OhtoGangg <ogealaviiva@gmail.com>
2025-09-05 01:23:15 +0300

    Update db.ts

--- StreamMonitor/server/db.ts
+++ StreamMonitor/server/db.ts
@@ -1,5 +1,12 @@
 import { createClient } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
 
-const client = createClient({ url: process.env.DATABASE_URL! });
+if (!process.env.DATABASE_URL) {
+  throw new Error("Missing DATABASE_URL environment variable");
+}
+
+const client = createClient({
+  url: process.env.DATABASE_URL,
+});
+
 export const db = drizzle(client);
