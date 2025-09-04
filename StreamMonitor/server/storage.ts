d102d9b431d500c5b69bfdd7ca39b46be314bdd9
OhtoGangg <ogealaviiva@gmail.com>
2025-09-05 01:16:08 +0300

    Update storage.ts

--- StreamMonitor/server/storage.ts
+++ StreamMonitor/server/storage.ts
@@ -5,38 +5,20 @@ import { eq } from "drizzle-orm";
 
 export const storage = {
   users: {
-    getById: async (id: string) => {
-      return await db.select().from(users).where(eq(users.id, id)).get();
-    },
-    create: async (data: { username: string; password: string }) => {
-      return await db.insert(users).values(data).get();
-    },
+    getById: async (id: string) => db.select().from(users).where(eq(users.id, id)).get(),
+    create: async (data: { username: string; password: string }) => db.insert(users).values(data).get(),
   },
   streamers: {
-    getAll: async () => {
-      return await db.select().from(streamers).all();
-    },
-    getByDiscordId: async (discordUserId: string) => {
-      return await db.select().from(streamers).where(eq(streamers.discordUserId, discordUserId)).get();
-    },
-    create: async (data: any) => {
-      return await db.insert(streamers).values(data).get();
-    },
-    update: async (discordUserId: string, data: any) => {
-      return await db.update(streamers).set(data).where(eq(streamers.discordUserId, discordUserId)).get();
-    },
+    getAll: async () => db.select().from(streamers).all(),
+    getByDiscordId: async (discordUserId: string) => db.select().from(streamers).where(eq(streamers.discordUserId, discordUserId)).get(),
+    create: async (data: any) => db.insert(streamers).values(data).get(),
+    update: async (discordUserId: string, data: any) => db.update(streamers).set(data).where(eq(streamers.discordUserId, discordUserId)).get(),
   },
   botSettings: {
-    get: async () => {
-      return await db.select().from(botSettings).get();
-    },
-    update: async (data: any) => {
-      return await db.update(botSettings).set(data).get();
-    },
+    get: async () => db.select().from(botSettings).get(),
+    update: async (data: any) => db.update(botSettings).set(data).get(),
   },
   activities: {
-    create: async (data: any) => {
-      return await db.insert(activities).values(data).get();
-    },
+    create: async (data: any) => db.insert(activities).values(data).get(),
   },
 };
